b34bc09eb35686989743b882031e1abf444b3fb8
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-19 19:24:02 +0600

    Created first task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число => ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста повторите ввод");
+    return;
+}
+System.Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10;
+System.Console.WriteLine($"Вторая цифра '{secondRank}'");
